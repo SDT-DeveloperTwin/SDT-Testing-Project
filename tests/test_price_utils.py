9c302ec7b3b311c739ab64fecd15e5520aaf94d3
mahmoud99-cell <62478103+mahmoud99-cell@users.noreply.github.com>
2025-07-25 20:55:56 +0200

    Add files via upload

---
+++ tests/test_price_utils.py
@@ -0,0 +1,9 @@
+# test_price_utils.py
+from project.price_utils import apply_discount
+
+def test_apply_discount():
+    price = 100
+    discount = 0.2
+    expected = 80
+    result = apply_discount(price, discount)
+    assert result == expected
