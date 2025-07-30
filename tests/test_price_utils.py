4a744a87109b0b06dec2e06d8eac1146f773d04d
mahmoud99-cell <62478103+mahmoud99-cell@users.noreply.github.com>
2025-07-30 14:19:57 +0200

    Update test_price_utils.py path

--- tests/test_price_utils.py
+++ tests/test_price_utils.py
@@ -1,4 +1,7 @@
 # test_price_utils.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from price_utils import apply_discount
 
 def test_apply_discount():
